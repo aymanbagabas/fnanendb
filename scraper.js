aymanbagabas/fnanendb

commit 26581276140f78753850c326e4bffb64b83995ad

scrape lyrics inside stronge tags

scraper.js
@@ -141,6 +141,13 @@ const debug = process.env.DEBUG;
 						node.children.forEach(n => {
 							if (n.type === 'text') {
 								lyrics += n.data + '\n';
+							} else if (n?.name === 'strong' || n?.name === 'span') {
+								const s = n?.children?.[0];
+								s?.children?.forEach(c => {
+									if (c?.type === 'text') {
+										lyrics += c.data + '\n';
+									}
+								})
 							}
 						})
 					})

.github/workflows/deploy.yml
@@ -28,7 +28,7 @@ jobs:
         with:
           node-version: ${{ matrix.node-version }}
 
-      - name: NPM install and run
+      - name: Run, commit, and push
         run: |
           mkdir $OUTPUT
           npm install
@@ -38,9 +38,12 @@ jobs:
           echo "NOW=$NOW" >> $GITHUB_ENV
           git config --global user.name $USERNAME
           git config --global user.email $USEREMAIL
-          git add $OUTPUT
-          git commit -m "fnanendb: autopublish $NOW"
-          git push origin HEAD:master
+          git update-index --refresh
+          if ! git diff-index --quiet HEAD --; then
+            git add -f $OUTPUT
+            git commit -m "fnanendb: autopublish $NOW"
+            git push origin HEAD:master
+          fi
 
       - name: Archive code coverage results
         uses: actions/upload-artifact@v2

commit af06116f5c35e4d794fa1cd74381b1662154dd65

fix scraping strong and span tags
Fixes: 26581276140f ("scrape lyrics inside stronge tags")

scraper.js
@@ -142,8 +142,7 @@ const debug = process.env.DEBUG;
 							if (n.type === 'text') {
 								lyrics += n.data + '\n';
 							} else if (n?.name === 'strong' || n?.name === 'span') {
-								const s = n?.children?.[0];
-								s?.children?.forEach(c => {
+								n?.children?.forEach(c => {
 									if (c?.type === 'text') {
 										lyrics += c.data + '\n';
 									}